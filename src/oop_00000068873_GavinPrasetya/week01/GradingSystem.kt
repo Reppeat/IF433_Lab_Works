01a335e9d53cd6bfa466c66143ece435eecdb765
Reppeat <gavinprasetya2003@gmail.com>
2026-02-06 19:42:32 +0700

    week01: initial setup and procedural code

--- src/oop_00000068873_GavinPrasetya/week01/GradingSystem.kt
+++
@@ -1,8 +0,0 @@
-package oop_00000068873_GavinPrasetya.week01
-
-fun main() {
-    var name: String = "Gavin Prasetya"
-    val score: Int =  80
-
-    println("Nama: " + name + ", Nilai: " + score)
-}
---
+++ src/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package week01
+
+fun main() {
+    var name: String = "Gavin"
+    var score: Int = 0
+
+    println("Nama:" + name + ", Nilai:" + score)
+}
