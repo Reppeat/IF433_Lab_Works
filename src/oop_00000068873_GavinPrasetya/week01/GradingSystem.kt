31a75d13a38b3c2e35d65a1f29f75c6af7ad0f5d
Reppeat <gavinprasetya2003@gmail.com>
2026-02-06 19:24:06 +0700

    week01: initial setup and procedural code

--- src/oop_00000068873_GavinPrasetya/week01/GradingSystem.kt
+++ src/oop_00000068873_GavinPrasetya/week01/GradingSystem.kt
@@ -1,28 +1,8 @@
 package oop_00000068873_GavinPrasetya.week01
 
 fun main() {
-    // refactor: gunakan val dan hapus tipe data eksplisit
-    val name = "Gavin Prasetya"
-    val score = 80
+    var name: String = "Gavin Prasetya"
+    val score: Int =  80
 
-    // string template
-    println("Nama: $name, Nilai: $score")
-
-    val grade = when (score) {
-        in 90..100 -> "A"
-        in 80..89 -> "B"
-        in 70..79 -> "C"
-        else -> "D"
-    }
-
-    println("Grade kamu: $grade")
-    println("Status: ${calculateStatus(score)}")
-
-    val studentId: String? = null
-    val idLength = studentId?.length ?: 0
-
-    println("Panjang ID: $studentId")
+    println("Nama: " + name + ", Nilai: " + score)
 }
-
-fun calculateStatus(score: Int) =
-    if (score > 75) "Lulus" else "Tidak Lulus"
