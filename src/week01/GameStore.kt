69c59ec07d2a992a9f510cbb502c2d33cfa85628
Reppeat <gavinprasetya2003@gmail.com>
2026-02-06 20:25:34 +0700

    week01: (tugas 2) implement discount logic

--- src/week01/GameStore.kt
+++ src/week01/GameStore.kt
@@ -1,11 +1,16 @@
 package week01
 
-val gameTitle = "Elden Ring"
-val price = 750_000
+fun main() {
 
-fun calculateDiscount(price: Int) =
-    if (price > 500_000)
-        price - (price * 20 / 100)
-    else
-        price - (price * 10 / 100)
+
+    val gameTitle = "Elden Ring"
+    val price = 750_000
+
+    // Fungsi diskon (Expression Body)
+    fun calculateDiscount(price: Int): Int =
+        if (price > 500_000)
+            price - (price * 20 / 100)
+        else
+            price - (price * 10 / 100)
+}
 
