0c1ee4be2cef1644381d9ffffa183c09ff2da0d3
Reppeat <gavinprasetya2003@gmail.com>
2026-02-06 19:10:07 +0700

    week01: add calculateStatus function

--- src/oop_00000068873_GavinPrasetya/week01/GradingSystem.kt
+++ src/oop_00000068873_GavinPrasetya/week01/GradingSystem.kt
@@ -1,19 +1,23 @@
 package oop_00000068873_GavinPrasetya.week01
 
 fun main() {
-    //refactor: gunakan val dan hapus tipe data eksplisit
-    val name: String = "Gavin Prasetya"
-    val score: Int =  80
+    // refactor: gunakan val dan hapus tipe data eksplisit
+    val name = "Gavin Prasetya"
+    val score = 80
 
-    //refactor string template ($name)
-    println("Nama:  $name, Nilai: $score")
+    // string template
+    println("Nama: $name, Nilai: $score")
 
     val grade = when (score) {
-        in 90 < 100 -> "A"
-        in 80 < 89 -> "B"
-        in 80 < 79 -> "C"
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
         else -> "D"
-
-    println("Grade kamu:  $grade")
     }
+
+    println("Grade kamu: $grade")
+    println("Status: ${calculateStatus(score)}")
 }
+
+fun calculateStatus(score: Int) =
+    if (score > 75) "Lulus" else "Tidak Lulus"
