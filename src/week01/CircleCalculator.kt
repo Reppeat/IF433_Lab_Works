cab19bed61e751a711427cd9e2d4d6ae358a4498
Reppeat <gavinprasetya2003@gmail.com>
2026-02-06 20:19:59 +0700

    week01: (tugas 1) clean up unused args and comments

--- src/week01/CircleCalculator.kt
+++ src/week01/CircleCalculator.kt
@@ -1,19 +1,14 @@
 package week01
 
-fun main(args: Array<String>) {
-// Variable definition
+fun main() {
+
     val radius = 7.0
     val pi = 3.14
-// Calculation
+
     var area: Double = pi * radius * radius
-
-    // Output Concatenation
     println("Radius: $radius , Area: $area")
-
-    // Logic check
     checkSize(area)
 }
-
 fun checkSize(area: Double) =
     if (area > 100) "This is a Big Circle"
     else "This is a Small Circle"
