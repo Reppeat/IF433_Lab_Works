4f02feb1a75e15107f73915cc5907364e0c374a5
Reppeat <gavinprasetya2003@gmail.com>
2026-02-09 14:45:41 +0700

    week02: add warning validation in init block

--- src/week02/Student.kt
+++ src/week02/Student.kt
@@ -1,21 +1,16 @@
 package week02
 
-class Student (
-    val name : String,
-    val nim : String,
-    var major : String,
-
+class Student(
+    val name: String,
+    val nim: String,
+    var major: String
 ) {
-    //body class kosong dulu
-}
-
-class Student (val name : String, val nim : String, var major : String){
     init {
-        if (nim.length !=5) {
-            println("WARNING: Objek tercipta dengan NIM  ($nim) yang tidak valid")
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid")
             println("Data mahasiswa $name mungkin akan bermasalah di sistem")
-        }else {
-            println("LOG: Objek student $name berhasil dialokasikan di memmory")
+        } else {
+            println("LOG: Objek student $name berhasil dialokasikan di memory")
         }
     }
 }
