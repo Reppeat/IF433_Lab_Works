a3590b189bc0b35fd4204800f7cd01ee3119506c
Reppeat <gavinprasetya2003@gmail.com>
2026-02-23 15:32:31 +0700

    week04: (task 2) test employeee hierarchy in main

--- src/week04/Main.kt
+++ src/week04/Main.kt
@@ -17,4 +17,13 @@ fun main() {
     myElectricCar.openTrunk()
     myElectricCar.honk()
     myElectricCar.accelerate()
+
+    println("\n--- Employee Hierarchy ---")
+    val manager = Manager(name = "Agus", baseSalary = 10000000)
+    manager.work()
+    println("Bonus Manager: Rp${manager.calculateBonus()}")
+
+    val developer = Developer(name = "Ani", baseSalary = 8000000, programmingLanguage = "Kotlin")
+    developer.work()
+    println("Bonus Developer: Rp${developer.calculateBonus()}")
 }
