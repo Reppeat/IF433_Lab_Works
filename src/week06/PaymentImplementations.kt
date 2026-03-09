7c2172eb536827c3089b47e878ac4a993eee05ac
Reppeat <gavinprasetya2003@gmail.com>
2026-03-09 15:41:12 +0700

    week06: implement decoupled processCheckout in main

--- src/week06/PaymentImplementations.kt
+++ src/week06/PaymentImplementations.kt
@@ -1,2 +1,13 @@
 package week06
 
+class Gopay : PaymentMethod {
+    override fun pay(amount: Double) {
+        println("Processing Rp$amount via Gopay Server")
+    }
+}
+
+class CreditCard : PaymentMethod {
+    override fun pay(amount: Double):  {
+    println("Contacting Bank for Rp$amount")
+    }
+}
