fc93d4ce7801e02059572f81e8a416763b560bc2
Reppeat <gavinprasetya2003@gmail.com>
2026-04-13 21:06:07 +0700

    week08: test safe casting in main

--- src/week08/OrderModels.kt
+++ src/week08/OrderModels.kt
@@ -3,4 +3,4 @@ package week08
 class City(val name: String)
 class Address(val city: City?)
 class DeliveryDetails(val address: Address?)
-class order (val delivery: DeliveryDetails?, val totalPrice: Int?)
+class Order (val deliveryDetails:DeliveryDetails?, val totalPrice: Int?)
