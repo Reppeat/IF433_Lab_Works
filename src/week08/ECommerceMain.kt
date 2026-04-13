0332131fc184774646ff8ff2f35c0c683f47890a
Reppeat <gavinprasetya2003@gmail.com>
2026-04-13 21:27:50 +0700

    week08: (task) test full e-commerce pipeline in main

--- src/week08/ECommerceMain.kt
+++ src/week08/ECommerceMain.kt
@@ -7,6 +7,6 @@ fun main() {
         mapOf("id" to "C01", "name" to "T-Shirt", "type" to "CLOTHING", "size" to "XL"),
         mapOf("id" to "E02", "name" to "Mouse", "type" to "ELECTRONIC", "warranty" to "Not An Integer"),
         mapOf("name" to "Ghost Item", "type" to "CLOTHING"),
-        mapOf("id" to "X01", "name" to "Unknown", "type" to "FOOD")
+        mapOf("id" to "X01", "name" to "Unknown", "type" to "FOODS")
     )
 }
