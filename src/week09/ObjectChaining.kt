81dbf0001cd80b609abb19768d432c397d2c04ee
Reppeat <gavinprasetya2003@gmail.com>
2026-04-21 21:24:11 +0700

    week09: chain filter, sortedBy, and map on object collection

--- src/week09/ObjectChaining.kt
+++ src/week09/ObjectChaining.kt
@@ -3,19 +3,21 @@ package week09
 data class Student(val name: String, val gpa: Double)
 
 fun main() {
-    val student = listOf(
+
+    val students = listOf(
         Student("Andi", 3.2),
         Student("Budi", 3.9),
         Student("Citra", 2.8),
         Student("Dewi", 3.7)
     )
+
     println("=== HONOR STUDENTS PIPELINE ===")
-    val honorNames =  students
-        .filter {it.gpa >= 3.5}
-        .sortedBy {it.name}
-        .map {it.name.uppercase()}
 
-    honorNames.forEach{println("Honor Roll: $sit")}
+    // Pipeline chaining operations
+    val honorNames = students
+        .filter { it.gpa >= 3.5 }        // 1. Ambil GPA >= 3.5
+        .sortedBy { it.name }           // 2. Urutkan berdasarkan nama
+        .map { it.name.uppercase() }    // 3. Ambil nama & uppercase
 
-
+    honorNames.forEach { println("Honor Roll: $it") }
 }
